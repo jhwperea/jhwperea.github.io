32ae0077c93975fe5bb6632fd58bc27f7352ce63
jhwperea <jhwperea@gmail.com>
2022-06-13 11:56:44 -0500

    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,46 +1,15 @@
 new Vue({
 	el:'#app',
 	data:{
-		skills:[
-			{ "description": "Vue JS", "src": "vue" },
-			{ "description": "Material Design", "src": "materialdesign" },
-			{ "description": "Bootstrap 4", "src": "bootstrap" },
-			{ "description": "Cordova", "src": "cordova" },
-			{ "description": "Laravel", "src": "laravel" },
-			{ "description": "WordPress", "src": "wordpress" },
-			{ "description": "Html 5", "src": "html5" },
-			{ "description": "Css 3", "src": "css3" },
-			{ "description": "Javascript", "src": "js" },
-			{ "description": "Php 7", "src": "php7" },
-			{ "description": "MySQL", "src": "mysql" },
-			{ "description": "jQuery", "src": "jquery" },
-		],
-		works:[
-			// Web + Local
-			{ "name": "Biokurk", "src": "biokurk", "url": "https://biokurk.com" },
-			{ "name": "Sirot", "src": "sirot", "url": "http://sirotsas.com" },
-			{ "name": "Registel", "src": "registel", "url": "http://registelcolombia.com" },
-			{ "name": "Acceautos", "src": "acceautos", "url": "http://acceautos.com.co" },
-			// Just Local
-			{ "name": "Difamm Ingeniería", "src": "difamm" },
-			{ "name": "TarjetasGrama", "src": "tarjetasgrama" },
-			{ "name": "Quirúrgicamente", "src": "quirurgicamente" },
-			{ "name": "Multipolimeros", "src": "multipolimeros" },
-			{ "name": "Orion", "src": "orion" },
-			{ "name": "Trazo al paso", "src": "trazoalpaso" },
-			{ "name": "Appsamblea", "src": "appsamblea" },
-			{ "name": "Awesome Heroes", "src": "awesomeheroes" },
-			{ "name": "Constructorio", "src": "constructorio" },
-			{ "name": "Go Rest", "src": "gorest" },
-			{ "name": "Atencion Integral", "src": "aic" },
-			{ "name": "Hispanic Tax", "src": "hispanic" },
-			{ "name": "Grazzia", "src": "grazzia" },
-			{ "name": "Isnandia", "src": "isnandia" },
-			// Remodeled by Other
-			// { "name": "Atencion Integral", "src": "aic", "url": "https://atencionintegralencasa.com.co" },
-			// { "name": "Hispanic Tax", "src": "hispanic", "url": "https://hispanictaxinc.com" },
-			// { "name": "Grazzia", "src": "grazzia", "url": "https://www.grazzia.com.co" },
-			// { "name": "Isnandia", "src": "isnandia", "url": "https://isnandia.com" },
-		]
+		skills: [],
+		works: []
+	},
+	mounted(){
+		this.getSkills();
+		this.getWorks()
+	},
+	methods: {
+		getSkills(){ axios('assets/js/skills.json').then(r=>{ this.skills = r.data }) },
+		getWorks(){ axios('assets/js/works.json').then(r=>{ this.works = r.data }) }
 	}
 });
