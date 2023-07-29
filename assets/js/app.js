66ad2dceea7de09fe50bc85a345267654966868f
jhwperea <jhwperea@gmail.com>
2023-07-29 16:35:03 -0500

    All new, Bootstrap 5

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,12 +4,26 @@ new Vue({
 		skills: [],
 		works: []
 	},
-	mounted(){
-		this.getSkills();
-		this.getWorks()
-	},
+	mounted(){ this.getData() },
 	methods: {
-		getSkills(){ axios('assets/js/skills.json').then(r=>{ this.skills = r.data }) },
-		getWorks(){ axios('assets/js/works.json').then(r=>{ this.works = r.data }) }
+		getData(){
+			axios('assets/js/skills.json').then(r=>{ this.skills = r.data });
+			axios('assets/js/works.json').then(r=>{ this.works = r.data })
+		},
 	}
+});
+$(document).ready(function(){
+	$(".owl-carousel").owlCarousel({
+		loop: true,
+		margin: 10,
+		nav: false,
+		autoplay: true,
+		autoplayTimeout: 1000,
+		responsiveClass: true,
+		responsive: {
+			0: { items: 3 },
+			600: { items: 6 },
+			1000: { items: 12 }
+		}
+	});
 });
