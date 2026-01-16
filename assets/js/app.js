b3d5a76b20fd1115ff1157799f440a8ee9bbd479
jhonier.perea <jperea@bluedataservice.com>
2026-01-16 06:52:47 -0500

    Improving data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,21 @@
-new Vue({
-	el:'#app',
-	data:{
-		skills: [],
-		works: []
+const app = Vue.createApp({
+	data() {
+		return {
+			skills: [],
+			works: [],
+			jobs: [],
+			viewworks: true,
+			viewskills: true,
+			viewjobs: false
+		}
 	},
-	mounted(){ this.getData() },
+	mounted() { this.getData() },
 	methods: {
-		getData(){
-			axios('assets/js/skills.json').then(r=>{ this.skills = r.data });
-			axios('assets/js/works.json').then(r=>{ this.works = r.data })
-		},
+		getData() {
+			axios('assets/js/skills.json').then(r => { this.skills = r.data });
+			axios('assets/js/works.json').then(r => { this.works = r.data });
+			axios('assets/js/jobs.json').then(r => { this.jobs = r.data });
+		}
 	}
 });
+app.mount('#app');
